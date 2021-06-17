159827e45b34441eb32aa94fe92c0bdcade800cf
edanurisik <5518123002@cumhuriyet.edu.tr>
2021-06-17 21:04:14 +0300

    Yüklendi

---
+++ js/que.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Covid-19 vakası Türkiye'de ilk ne zaman görüldü?",
+    answer: "11 Mart 2020",
+    options: [
+      "11 Mart 2020",
+      "15 Mayıs 2020" ,
+      "08 Ocak 2021",
+      "02 Şubat 2018"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Enfeksiyondan korunma önlemlerinden aşağıdakilerden hangisi değildir?",
+    answer: "Kullandığımız mendili tekrar kullanmak",
+    options: [
+      "Temizlik",
+      "Kullandığımız mendili tekrar kullanmak",
+      "Mesafe",
+      "Elleri sabun ve suyla 20 sn yıkamak",
+      "Maske"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Ülkemizde Covid-19 vakasından ilk ölüm hangi tarihte gerçekleşti?",
+    answer: "15 Mart 2020",
+    options: [
+      "15 Haziran 2020",
+      "15 Aralık 2020",
+      "15 Mart 2020"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Videoda bahsedilen maske kullanımı hakkında verilen bilgilerden aşağıdakilerden hangisi yanlıştır?",
+    answer: "Maske burun, ağız ve çenenin hepsini kapatmasına gerek yoktur,sadece ağzı kapatması yeterlidir.",
+    options: [
+      "Maskeyi takmadan önce eller en az 20 sn sabun ve suyla yıkanmalıdır. ",
+      "Maske çıkarılırken ön kısmına dokunmadan ipler yardımıyla çıkarılıp atılmalıdır.",
+      "Maske burun, ağız ve çenenin hepsini kapatmasına gerek yoktur,sadece ağzı kapatması yeterlidir.",
+      "Su ve sabun olmadığı durumlarda eller 20-30 sn alkol içerikli antiseptik ile ovalanmalıdır."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Aşağıdaki resimdeki kişi kimdir?",
+    answer: "Sağlık Bakanı / Fahrettin Koca",
+    options: [
+      "Milli Eğitim Bakanı / Ziya Selçuk",
+      "Sanayi ve Teknoloji Bakanı / Mustafa Varank",
+      "Sağlık Bakanı / Fahrettin Koca",
+      "Milli Savunma Bakanı / Hulusi Akar",
+      "İçişleri Bakanı / Süleyman Soylu"
+    ]
+  },
+];
